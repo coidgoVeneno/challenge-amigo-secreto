2b437cd28f2830a25a68ba1550e74a433fbef090
Gerardo Ramirez <geramir27@gmail.com>
2025-01-31 13:51:32 -0600

    Creado la funcion para crear la lista y llenado de array

--- app.js
+++ app.js
@@ -1,2 +1,29 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let agregaNuevoAmigo = []
+let lista = document.querySelector(".name-list")
+let amigo = document.querySelector(".input-name")
+
+function agregarAmigo (){
+    lista.innerHTML = ""
+
+    agregaNuevoAmigo.push(amigo.value)
+    console.log(agregaNuevoAmigo)
+
+    for (let i = 0; i < agregaNuevoAmigo.length; i++) {
+
+        const nuevoAmigo = document.createElement("li")
+        nuevoAmigo.textContent = agregaNuevoAmigo[i];
+        
+        amigo.value = ""
+
+
+
+        lista.appendChild(nuevoAmigo)
+
+        console.log(nuevoAmigo)
+        
+    }
+
+    
+
+}
