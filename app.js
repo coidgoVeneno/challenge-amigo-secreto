921e29ef4a21d44fa88bea66269217c51830667b
Gerardo Ramirez <geramir27@gmail.com>
2025-01-31 21:40:23 -0600

    Refactorizando el projecto

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-let agregaNuevoAmigo = []
+let listaDeAmigos = []
 let lista = document.querySelector(".name-list")
 let amigo = document.querySelector(".input-name")
 
-function agregarAmigo (){
+/* function agregarAmigo (){
     lista.innerHTML = ""
 
     agregaNuevoAmigo.push(amigo.value)
@@ -22,8 +22,38 @@ function agregarAmigo (){
 
         console.log(nuevoAmigo)
         
-    }
+    }    
 
+}*/
+
+// Refactorizando la funcion de agregar amigos
+
+function agregarAmigo(){
+    if(validarNombre(amigo.value)){
+        if(listaDeAmigos.length<5){
+            lista.innerHTML = "";    
+         listaDeAmigos.push(amigo.value);
+        //console.log(listaDeAmigos)
+        listaDeAmigos.forEach(li => {
+            const listas = document.createElement("li");
+            listas.textContent = li;
+            lista.appendChild(listas)
+            //console.log(listas)       
+            
+        });    
+        } else{
+            console.log("Exediste el limete de la lista, es hora de elegir un amigo.")
+        }
     
-
+        amigo.value=""; 
+    }   else{
+        console.log("El nombre no es valido solo letras de el alfabeto seran admitidas tampoco los espacios seran permitidos")
+    }
 }
+
+function validarNombre (nombre){
+    const regex = /^[A-Za-zÁáÉéÍíÓóÚúÜüÑñ'-]{2,50}$/;
+  return regex.test(nombre);
+}
+
+//console.log(validarNombre("Satoru"))
